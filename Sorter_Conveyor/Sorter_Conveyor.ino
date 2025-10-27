2decdbe0b0c671470eb77b00c925cc2f7cd430aa
Yasiru <yasirunipunbasnayake@gmail.com>
2025-10-28 02:55:54 +0530

    Add sorter motor state machine for alternating cycles
    Implemented a state machine for the sorter motor to alternate between forward and backward cycles with pauses in between. The conveyor motor continues to run at a constant speed. This improves control and allows for more flexible operation of the sorter mechanism.

--- Sorter_Conveyor/Sorter_Conveyor.ino
+++ Sorter_Conveyor/Sorter_Conveyor.ino
@@ -7,7 +7,23 @@
 #define CONVEYOR_DIR_PIN 4    // Direction pin (DIR) for Conveyor motor
 
 // Motion parameters
-const int delayTime = 200;  // Microseconds between pulses (controls speed)
+const int stepsPerRev = 200;   // Steps per revolution for sorter
+const int delayTime = 200;     // Microseconds between pulses (controls speed)
+int Rev = 8;                   // Number of revolutions for sorter
+
+// State tracking for sorter
+enum SorterState {
+  MOVING_FORWARD,
+  PAUSE_AFTER_FORWARD,
+  MOVING_BACKWARD,
+  PAUSE_AFTER_BACKWARD
+};
+
+SorterState sorterState = MOVING_FORWARD;
+long sorterStepCount = 0;
+long sorterTargetSteps = stepsPerRev * Rev;
+unsigned long pauseStartTime = 0;
+const unsigned long pauseDuration = 1000; // 1 second pause
 
 void setup() {
   // Set pin modes
@@ -17,20 +33,66 @@ void setup() {
   pinMode(CONVEYOR_DIR_PIN, OUTPUT);
 
   Serial.begin(115200);
-  Serial.println("Sorter and Conveyor motors rotating at constant speed...");
+  Serial.println("Sorter: alternating cycles | Conveyor: constant speed");
 
-  // Set directions (change HIGH/LOW to reverse)
-  digitalWrite(SORTER_DIR_PIN, HIGH);
-  digitalWrite(CONVEYOR_DIR_PIN, HIGH);
+  // Set initial directions
+  digitalWrite(SORTER_DIR_PIN, HIGH);    // Start forward
+  digitalWrite(CONVEYOR_DIR_PIN, HIGH);  // Constant direction
 }
 
 void loop() {
-  // Generate pulses for both motors simultaneously
-  digitalWrite(SORTER_STEP_PIN, HIGH);
+  // Handle sorter motor state machine
+  switch (sorterState) {
+    case MOVING_FORWARD:
+      if (sorterStepCount < sorterTargetSteps) {
+        digitalWrite(SORTER_STEP_PIN, HIGH);
+        delayMicroseconds(delayTime);
+        digitalWrite(SORTER_STEP_PIN, LOW);
+        delayMicroseconds(delayTime);
+        sorterStepCount++;
+      } else {
+        Serial.println("Sorter: Forward complete, pausing...");
+        sorterState = PAUSE_AFTER_FORWARD;
+        pauseStartTime = millis();
+        sorterStepCount = 0;
+      }
+      break;
+
+    case PAUSE_AFTER_FORWARD:
+      if (millis() - pauseStartTime >= pauseDuration) {
+        Serial.println("Sorter: Moving backward...");
+        digitalWrite(SORTER_DIR_PIN, LOW);  // Reverse direction
+        sorterState = MOVING_BACKWARD;
+      }
+      break;
+
+    case MOVING_BACKWARD:
+      if (sorterStepCount < sorterTargetSteps) {
+        digitalWrite(SORTER_STEP_PIN, HIGH);
+        delayMicroseconds(delayTime);
+        digitalWrite(SORTER_STEP_PIN, LOW);
+        delayMicroseconds(delayTime);
+        sorterStepCount++;
+      } else {
+        Serial.println("Sorter: Backward complete, pausing...");
+        sorterState = PAUSE_AFTER_BACKWARD;
+        pauseStartTime = millis();
+        sorterStepCount = 0;
+      }
+      break;
+
+    case PAUSE_AFTER_BACKWARD:
+      if (millis() - pauseStartTime >= pauseDuration) {
+        Serial.println("Sorter: Moving forward...");
+        digitalWrite(SORTER_DIR_PIN, HIGH);  // Forward direction
+        sorterState = MOVING_FORWARD;
+      }
+      break;
+  }
+
+  // Conveyor motor runs continuously
   digitalWrite(CONVEYOR_STEP_PIN, HIGH);
   delayMicroseconds(delayTime);
-
-  digitalWrite(SORTER_STEP_PIN, LOW);
   digitalWrite(CONVEYOR_STEP_PIN, LOW);
   delayMicroseconds(delayTime);
 }
